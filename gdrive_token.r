087874a8e31172f76bc2ef8ee0dcf6cf557de4af
meteoclima <39494694+meteoclima@users.noreply.github.com>
2025-05-16 15:55:50 +0200

    Delete gdrive_token.r

--- gdrive_token.r
+++
@@ -1,6 +0,0 @@
-library(googledrive)
-drive_auth()
-# Ti si apre una finestra browser per autorizzare l'accesso a Google Drive.
-# Dopo aver autorizzato:
-token <- drive_token()
-saveRDS(token, "C:/test/gdrive_token.rds")
