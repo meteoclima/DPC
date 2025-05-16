047caaec2690b5c82279e0d64639792a13ef10a8
meteoclima <39494694+meteoclima@users.noreply.github.com>
2025-05-16 16:48:26 +0200

    Update upload_to_drive.r

--- upload_to_drive.r
+++ upload_to_drive.r
@@ -1,16 +1,22 @@
 library(googledrive)
+library(base64enc)
 
-# Define the folder ID from your Google Drive
-drive_folder_id <- "1o-1bHJ3nzNEEj1M8eY468BPKZkJXpyYj?hl=it"  # Replace this with your folder ID
+# Read the base64-encoded token from the GitHub secret
+token_b64 <- Sys.getenv("GDRIVE_PAT")  # Get token from the secret
+token_path <- tempfile(fileext = ".rds")
+writeBin(base64decode(token_b64), token_path)  # Decode the base64 string to the .rds token file
 
 # Authenticate using the token
-drive_auth()
+drive_auth(token = token_path)
 
-# List the files you want to upload (assuming they are in the 'aggregati' folder)
+# Folder ID where you want to upload the files
+drive_folder_id <- "1o-1bHJ3nzNEEj1M8eY468BPKZkJXpyYj?hl=it"  # Replace with your actual folder ID
+
+# Directory to upload from
 local_dir <- "./DPC/aggregati"
 files <- list.files(local_dir, pattern = "\\.tif$", full.names = TRUE)
 
-# Upload each file to Google Drive
+# Upload each file to the Google Drive folder
 for (file in files) {
   message("Uploading ", basename(file), " to Google Drive folder with ID: ", drive_folder_id)
   drive_upload(media = file, path = as_id(drive_folder_id), overwrite = TRUE)
