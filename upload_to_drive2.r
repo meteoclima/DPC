06df49a4e8ab179cdca8b41cded68cd433eca583
meteoclima <39494694+meteoclima@users.noreply.github.com>
2026-01-16 21:03:26 +0100

    Update upload_to_drive2.r

--- upload_to_drive2.r
+++ upload_to_drive2.r
@@ -1,22 +1,22 @@
 library(googledrive)
 
-# Autenticazione tramite OAuth JSON dal secret GitHub
+# Configura OAuth client
 json_token <- Sys.getenv("GDRIVE_OAUTH_JSON")
 if(json_token == "") stop("GDRIVE_OAUTH_JSON vuoto!")
+
 token_path <- tempfile(fileext = ".json")
 writeLines(json_token, token_path)
-on.exit(unlink(token_path))  # cancella file alla fine
+on.exit(unlink(token_path))
 
-drive_auth(path = token_path)
+drive_auth_configure(path = token_path)
+drive_auth()
 
-# Cartella Drive
 drive_folder_id <- Sys.getenv("GDRIVE_FOLDER_ID")
 
-# Cartella locale
 local_dir <- "./METEO/CSV"
+
 dir.create(local_dir, showWarnings = FALSE, recursive = TRUE)
 
-# Upload CSV
 files <- list.files(local_dir, pattern = "\\.csv$", full.names = TRUE)
 for(file in files){
   message("Uploading ", basename(file))
@@ -26,5 +26,3 @@ for(file in files){
     overwrite = TRUE
   )
 }
-
-
