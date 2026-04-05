65e0f7bba0d3daa91b65a3c3f1c6ec7ada1caf6c
meteoclima <39494694+meteoclima@users.noreply.github.com>
2026-04-05 17:24:11 +0200

    Update empty_trash.R

--- empty_trash.R
+++ empty_trash.R
@@ -7,19 +7,35 @@ drive_auth(path = json_path)
 
 # Delete files older than 30 days
 folder_id <- "1o-1bHJ3nzNEEj1M8eY468BPKZkJXpyYj"
+
+# List files
 files <- drive_ls(as_id(folder_id))
 
-# Calculate date 30 days ago
-cutoff_date <- Sys.Date() - 30
-
-old_files <- files[as.Date(files$drive_resource$modifiedTime) < cutoff_date, ]
-
-if (nrow(old_files) > 0) {
-  message("Deleting ", nrow(old_files), " old files...")
-  for (i in 1:nrow(old_files)) {
-    message("Deleting: ", old_files$name[i])
-    drive_rm(old_files$id[i])
+# Check if there are files
+if (nrow(files) > 0) {
+  # Calculate date 30 days ago
+  cutoff_date <- Sys.Date() - 30
+  
+  # Get modification dates
+  mod_dates <- as.Date(sapply(files$drive_resource, function(x) x$modifiedTime))
+  
+  # Find old files
+  old_indices <- which(mod_dates < cutoff_date)
+  
+  # Delete old files if any
+  if (length(old_indices) > 0) {
+    old_files <- files[old_indices, ]
+    message("Deleting ", nrow(old_files), " old files...")
+    
+    for (i in 1:nrow(old_files)) {
+      message("Deleting: ", old_files$name[i])
+      drive_rm(old_files$id[i], forever = TRUE)
+    }
+    
+    message("Done!")
+  } else {
+    message("No old files to delete")
   }
 } else {
-  message("No old files to delete")
+  message("No files found in folder")
 }
