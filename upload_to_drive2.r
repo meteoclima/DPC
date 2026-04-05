c5a03be0b81413f8fd2b55044d69eb93fa882478
meteoclima <39494694+meteoclima@users.noreply.github.com>
2026-04-05 19:45:29 +0200

    Update upload_to_drive2.r

--- upload_to_drive2.r
+++ upload_to_drive2.r
@@ -1,13 +1,14 @@
 
 library(googledrive)
+library(base64enc)
 
-# Write the JSON key from environment variable to a temp file
-json_key <- Sys.getenv("GDRIVE_SERVICE_ACCOUNT_JSON")
-json_path <- tempfile(fileext = ".json")
-writeLines(json_key, json_path)
+# Leggi il token Base64 dal GitHub Secret
+token_b64 <- Sys.getenv("GDRIVE_TOKEN_B64")
+token_path <- tempfile(fileext = ".rds")
+writeBin(base64decode(token_b64), token_path)
 
-# Authenticate using the service account JSON key
-drive_auth(path = json_path)
+# Autentica con il tuo account personale (usa i tuoi 200GB)
+drive_auth(token = readRDS(token_path))
 
 # Google Drive folder ID (remove any URL query parameters)
 drive_folder_id <- "1o-1bHJ3nzNEEj1M8eY468BPKZkJXpyYj"
